e387206af3808565f6549384e61424b524d62697
Choejiwon <jiwon0929@ajou.ac.kr>
2019-12-02 14:00:14 +0900

    Initial finderboard.vue

--- src/views/TableList.vue
+++ src/views/TableList.vue
@@ -3,16 +3,15 @@
     <v-layout justify-center wrap>
       <v-flex md12>
         <material-card color="green" title="발견 게시판" text="실종견을 발견한 사람들의 게시글입니다.">
-          <v-data-table :headers="headers" :items="items" hide-actions>
+          <v-data-table :headers="headers" :listArray="this.listArray" hide-actions>
             <template slot="headerCell" slot-scope="{ header }">
               <span class="subheading font-weight-light text-success text--darken-3" v-text="header.text"/>
             </template>
-            <!-- <template slot="items" slot-scope="{ item }"> -->
-            <template slot="listArray" slot-scope="{ list }">
+            <template slot="listArray" slot-scope="{ list }" v-if="this.listArray">
               <td>{{ list.writer }}</td>
               <td>{{ list.title }}</td>
               <td>{{ list.writer }}</td>
-              <td class="text-xs-right">{{ item.place }}</td>
+              <td class="text-xs-right">{{ list.findPlace }}</td>
             </template>
           </v-data-table>
         </material-card>
@@ -47,48 +46,15 @@ export default {
         align: 'right'
       }
     ],
-    items: [
-      {
-        name: 'Dakota Rice',
-        country: 'Niger',
-        city: 'Oud-Tunrhout',
-        salary: '$35,738'
-      },
-      {
-        name: 'Minerva Hooper',
-        country: 'Curaçao',
-        city: 'Sinaai-Waas',
-        salary: '$23,738'
-      }, {
-        name: 'Sage Rodriguez',
-        country: 'Netherlands',
-        city: 'Overland Park',
-        salary: '$56,142'
-      }, {
-        name: 'Philip Chanley',
-        country: 'Korea, South',
-        city: 'Gloucester',
-        salary: '$38,735'
-      }, {
-        name: 'Doris Greene',
-        country: 'Malawi',
-        city: 'Feldkirchen in Kārnten',
-        salary: '$63,542'
-      }, {
-        name: 'Mason Porter',
-        country: 'Chile',
-        city: 'Gloucester',
-        salary: '$78,615'
-      }
-    ],
     listArray: []
   }),
   created() {
     this.$http.get('/finderboard')
       .then(response => {
-        if(JSON.stringify(response.data.success) === true){
+        if(JSON.stringify(response.data.success) === "true"){
           alert("response"+JSON.stringify(response.data.success)) 
           this.listArray = response.data.finderboards;
+          console.log(this.listArray);
         }else{
           alert("로그인이 필요합니다." + response.data.message)
         }
