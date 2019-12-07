e6cf692686671574cd89f7170e5eb3db9c0baea5
Choejiwon <jiwon0929@ajou.ac.kr>
2019-12-07 14:24:10 +0900

    Add Delete function(including authentication)

--- src/views/ShowFindPost.vue
+++ src/views/ShowFindPost.vue
@@ -6,20 +6,9 @@
           <v-form>
             <v-container py-0>
               <v-layout wrap column>
-                <!-- <v-row align="center" justify="center"> -->
                 <v-flex xs12 md4>
-                  <!-- <v-img
-                    src="https://picsum.photos/id/11/500/300"
-                    lazy-src="https://picsum.photos/id/11/10/6"
-                    aspect-ratio="1"
-                    class="grey lighten-2"
-                    max-width="500"
-                    max-height="300"
-                    :justify="center"
-                    ></v-img> -->
                     <strong class="display-3">{{findpost.title}}</strong>
                 </v-flex>
-                <!-- </v-row> -->
                 <v-flex xs12 md4>
                   <strong class="display-1 right" >{{findpost.writer}}</strong>
                 </v-flex>
@@ -30,15 +19,12 @@
                 </v-flex>
                 <v-flex xs12 md6>
                   <strong class="title right" >견종: {{findpost.petType}}</strong>
-                  <!-- <v-text-field label="Adress" class="purple-input"/> -->
                 </v-flex>
                 <v-flex xs12 md4>
                   <strong class="title right" >발견장소: {{findpost.findPlace}}</strong>
-                  <!-- <v-text-field label="City" class="purple-input"/> -->
                 </v-flex>
                 <v-flex xs12 md4>
-                  <strong class="title right" >발견일시: {{findpost.created}}</strong>
-                  <!-- <v-text-field label="Country" class="purple-input"/> -->
+                  <strong class="title right" >발견일시: {{findpost.findDate}}</strong>
                 </v-flex>
                 <v-divider></v-divider>
                 <v-divider></v-divider>
@@ -47,6 +33,7 @@
                     :items="items" 
                     :items-per-page="5" 
                     class="elevation-1" 
+                    color="black"
                     :search="search"
                     loading-text="Loading... Please wait">
                     <template slot="headerCell" slot-scope="{ header }">
@@ -56,17 +43,110 @@
                     <td>{{ item.writer }}</td>
                     <td>{{ item.body }}</td>
                     <td class="text-xs-right" outlined>{{ item.created }}</td>
-                    <v-btn @click="onDelete(item._id, item)" right>
-                        <v-icon>mdi-delete</v-icon>
+                    <v-divider vertical></v-divider>
+                    <v-btn
+                      color="error"
+                      dark
+                      @click.stop="commentDeleteDialog = true"
+                    >
+                      <v-icon color="white">mdi-delete</v-icon>
                     </v-btn>
+
+                    <v-dialog
+                      v-model="commentDeleteDialog"
+                      max-width="290"
+                    >
+                      <v-card>
+                        <v-card-title class="headline">댓글 삭제</v-card-title>
+
+                        <v-card-text>
+                          해당 댓글을 삭제하시겠습니까?
+                        </v-card-text>
+
+                        <v-card-actions>
+                          <v-spacer></v-spacer>
+
+                          <v-btn
+                            color="green darken-1"
+                            text
+                            @click="onCommentDelete(item._id, item)"
+                          >
+                            삭제
+                          </v-btn>
+
+                          <v-btn
+                            color="green darken-1"
+                            text
+                            @click="commentDeleteDialog = false"
+                          >
+                            취소
+                          </v-btn>
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>
                     </template>
                 </v-data-table>
                 <v-flex xs12 md6>
-                  <v-textarea outlined label="댓글 남기기" class="purple-input" v-model="newComment.body"/>
-                  <v-btn @click="clicked" right>작성 완료</v-btn>
+                  <v-textarea solo label="댓글 남기기" class="purple-input" v-model="newComment.body" rows="2"/>
+                  <v-divider vertical></v-divider>
+                  <v-btn @click="onCommentCreate" class="success">작성 완료</v-btn>
                 </v-flex>
                 <v-flex xs12 text-xs-right>
                   <v-btn class="mx-0 font-weight-light" color="success">게시글 수정</v-btn>
+                  <v-divider vertical></v-divider>
+                  <v-btn
+                      color="error"
+                      dark
+                      @click.stop="postDeleteDialog = true"
+                    >
+                    게시글 삭제
+                    </v-btn>
+
+                    <v-dialog
+                      v-model="postDeleteDialog"
+                      max-width="290"
+                    >
+                      <v-card>
+                        <v-card-title class="headline">게시글 삭제</v-card-title>
+
+                        <v-card-text>
+                          해당 게시글을 삭제하시겠습니까?
+                        </v-card-text>
+
+                        <v-card-actions>
+                          <v-spacer></v-spacer>
+
+                          <v-btn
+                            color="green darken-1"
+                            text
+                            @click="onPostDelete(findpost._id)"
+                          >
+                            삭제
+                          </v-btn>
+
+                          <v-btn
+                            color="green darken-1"
+                            text
+                            @click="postDeleteDialog = false"
+                          >
+                            취소
+                          </v-btn>
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>
+                    <v-snackbar
+                      v-model="postSnackbar"
+                    >
+                      삭제할 권한이 없습니다.
+                      <v-btn
+                        color="pink"
+                        text
+                        @click="postSnackbar = false"
+                      >
+                        Close
+                      </v-btn>
+                    </v-snackbar>
+                  <!-- <v-btn @click="onPostDelete(findpost._id)" class="mx-0 font-weight-light" color="error">게시글 삭제</v-btn> -->
                 </v-flex>
               </v-layout>
             </v-container>
@@ -100,11 +180,15 @@ export default {
                 body: '',
                 created: '',
             },
-            items: []
+            items: [],
+            postDeleteDialog: false,
+            commentDeleteDialog: false,
+            commentSnackbar: false,
+            postSnackbar: false,
         }
     },
     methods:{
-        clicked: function(){
+        onCommentCreate: function(){
             const today = new Date();
             const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
             const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
@@ -121,18 +205,42 @@ export default {
                     alert(err);
                 })
         },
-        onDelete: function(id, item){
+        onCommentDelete: function(id, item){
             const comment_id = id;
             const post_id = this.$route.params._id;
             const user_id = this.$store.state.userInfo._id;
 
-            console.log("c id : " + comment_id)
-            console.log("p id : " + post_id)
-            console.log("u id : " + user_id)
+            this.commentDeleteDialog = false
 
             this.$http.delete(`/finderboard/${post_id}/comments/${comment_id}`)
+            this.$router.push(`/showfindpost/${post_id}`)
+        },
+        onPostDelete: function(id){
+            const post_id = id;
+            const user_id = this.$store.state.userInfo._id;
+            const user_ID = this.$store.state.userInfo.ID;
+            const post_writer_ID = this.findpost.id;
+
+            console.log(this.$store.state.userInfo._id);
+            console.log("user ID"+this.$store.state.userInfo.ID);
+            console.log("post id: " + this.findpost.id)
+            console.log("writer: " + this.findpost.writer)
+            console.log("user name: " + this.$store.state.userInfo.name)
+            console.log("p id : " + post_id)
+            console.log("u _id : " + user_id)
+
+            if(user_ID === post_writer_ID){
+              this.$http.delete(`/finderboard/${post_id}`)
+                .then( (response) => {
+                  if(JSON.stringify(response.data.success) === "true"){
+                    this.$router.push(`/finderboard`);
+                  }
+                })
+            }
+            else{
+              this.postSnackbar = true;
+            }
         }
-        
     },
 }
 </script>
